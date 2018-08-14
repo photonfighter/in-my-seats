7c6cdd293a189c8d2d272df0f67f4345fc35b7ed
Max Walters <max.walters10@gmail.com>
2018-08-14 01:13:19 -0400

    Reworking repo

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# in-my-seats
-An Alexa Skill that tells you information about your Senate and House of Representatives
+# In My Seat (TITLE SUBJECT TO CHANGE)
+An Alexa Skill that tells you which House and Senate members represent you + some information about them
+
+# API Key
+You'll get it if deemed necessary for development 🤷
+
+### What to do with API Key
+1. Create a file in the root repo folder called *config.js*
+2. Structure *config.js* like shown below
+```javascript
+var config = {
+  API_KEY : [API KEY GOES HERE IN SINGLE QUOTES]
+}
+```
+3. Make sure you have the .gitignore
---
+++ api.js
@@ -0,0 +1,25 @@
+// Current testing fetch() requests are set to Miami University area/District 53 in Ohio
+import config from './config.js';
+
+let apikey = config.API_KEY;
+
+var header = new Headers();
+header.append("X-API-Key: " + apikey);
+
+function listSenateMembers(){
+  fetch('https://api.propublica.org/congress/v1/members/115/OH/current.json').then(function(response){
+    return response.json();
+  }).then(function(myJson) {
+    console.log(myJson);
+  });
+}
+
+function listHouseMembers(){
+  fetch('https://api.propublica.org/congress/v1/members/115/OH/53/current.json').then(function(response){
+    return response.json();
+  }).then(function(myJson) {
+    console.log(myJson);
+  });
+}
+
+console.log(listSenateMembers());
