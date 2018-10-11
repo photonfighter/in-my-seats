25d71c750a10e8e1e3d9b871d21212aa4641fa05
Max Walters <max.walters10@gmail.com>
2018-10-10 21:05:04 -0400

    I wasn't getting upset on line 25, you were

--- api.js
+++ api.js
@@ -1,23 +1,51 @@
 // Current testing fetch() requests are set to Miami University area/District 53 in Ohio
 import config from './config.js';
+import 'isomorphic-fetch';
+import './../console-dot-emoji/console.emoji.js'
 
-let apikey = config.API_KEY;
+let oxfordSURL = 'https://api.propublica.org/congress/v1/members/115/OH/current.json';
+let oxfordHURL = 'https://api.propublica.org/congress/v1/members/115/OH/53/current.json';
+let PROPUBLICA_API_KEY = config.API_KEY;
 
-var header = new Headers();
-header.append("X-API-Key: " + apikey);
+var ppHeader = new Headers('X-API-Key' : PROPUBLICA_API_KEY);
+
+var myInit = {
+    method: 'GET',
+    headers: {
+        ppHeader
+    },
+    mode: 'basic',
+    cache: 'default'
+};
 
 function listSenateMembers(){
-  fetch('https://api.propublica.org/congress/v1/members/115/OH/current.json').then(function(response){
-    return response.json();
-  }).then(function(myJson) {
-    console.log(myJson);
-  });
+  fetch(oxfordSURL, myInit)
+    .then(function(response) {
+         if (response.status == 401){
+             console.poo("Wow, I really fucked up, didn't I? Error code: " + response.status);
+             return;
+         } else {
+             console.beer("At least we fixed that problem...");
+
+    })
+    .then(function(myJson) {
+    console.rainbow(JSON.stringify(myJson));
+    });
 }
 
 function listHouseMembers(){
-  fetch('https://api.propublica.org/congress/v1/members/115/OH/53/current.json').then(function(response){
-    return response.json();
-  }).then(function(myJson) {
-    console.log(myJson);
-  });
+  fetch(oxfordHURL, myInit)
+    .then(function(response){
+        if (response.status == 401){
+            console.poo("Wow, I really fucked up, didn't I? Error code: " + response.status);
+            return;
+        } else {
+            console.beer("At least we fixed that problem...");
+
+    })
+    .then(function(myJson) {
+        console.rainbow(JSON.stringify(myJson));
+    });
 }
+
+listSenateMembers();
