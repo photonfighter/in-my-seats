d95204f8efb5303e437abcf4ed9e43196f144371
Max Walters <max.walters10@gmail.com>
2018-10-10 21:39:24 -0400

    note about axios calls

--- api.js
+++ api.js
@@ -1,31 +1,32 @@
 // Current testing fetch() requests are set to Miami University area/District 53 in Ohio
 import config from './config.js';
-import 'isomorphic-fetch';
+import 'isomorphic-fetch'; // LOOK INTO SWITCHING TO AXIOS CALLS
 import './../console-dot-emoji/console.emoji.js'
 
-let oxfordSURL = 'https://api.propublica.org/congress/v1/members/115/OH/current.json';
-let oxfordHURL = 'https://api.propublica.org/congress/v1/members/115/OH/53/current.json';
+let oxfordSURL = 'https://api.propublica.org/congress/v1/members/senate/OH/current.json';
+let oxfordHURL = 'https://api.propublica.org/congress/v1/members/house/OH/8/current.json';
 let PROPUBLICA_API_KEY = config.API_KEY;
 
-var ppHeader = new Headers('X-API-Key', PROPUBLICA_API_KEY);
+const ppHeader = {'X-API-Key' : PROPUBLICA_API_KEY};
+const publicaHeaders = new Headers(ppHeader);
 
 var myInit = {
     method: 'GET',
     headers: {
-        ppHeader
+        publicaHeaders
     },
     mode: 'basic',
     cache: 'default'
 };
 
-function listSenateMembers(){
+function listSenateMembers(){ // LOOK INTO SWITCHING TO AXIOS CALLS
   fetch(oxfordSURL, myInit)
     .then(function(response) {
          if (response.status == 401){
              console.poo("Wow, I really fucked up, didn't I? Error code: " + response.status);
              return;
          } else {
-             console.beer("At least we fixed that problem...");
+             console.beer("Hey, we fixed that issue!");
              return;
          }
     })
@@ -34,14 +35,14 @@ function listSenateMembers(){
     });
 }
 
-function listHouseMembers(){
+function listHouseMembers(){ // LOOK INTO SWITCHING TO AXIOS CALLS
   fetch(oxfordHURL, myInit)
     .then(function(response){
         if (response.status == 401){
             console.poo("Wow, I really fucked up, didn't I? Error code: " + response.status);
             return;
         } else {
-            console.beer("At least we fixed that problem...");
+            console.beer("Hey, we fixed that issue!");
             return;
         }
     })
@@ -50,4 +51,4 @@ function listHouseMembers(){
     });
 }
 
-listSenateMembers();
+listHouseMembers();
