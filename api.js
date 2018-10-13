6759fa9cc7a63b7ab88a37b5f3bfaba3cbf66f33
Max Walters <max.walters10@gmail.com>
2018-10-13 14:14:03 -0400

    THEY WORK

--- api.js
+++ api.js
@@ -1,18 +1,31 @@
-// Current testing requests are set to Miami University area/District 53 in Ohio
-import './../console-dot-emoji/console.emoji.js'
+// Current testing requests are set to Miami University area/District 8 in Ohio
 import 'isomorphic-fetch'
 
-const oxfordSURL = 'https://api.propublica.org/congress/v1/members/senate/OH/current.json';
-const oxfordHURL = 'https://api.propublica.org/congress/v1/members/house/OH/8/current.json';
 const config = require('./config.js');
 const PROPUBLICA_API_KEY = config.API_KEY;
+const oxfordSURL = 'https://api.propublica.org/congress/v1/members/senate/OH/current.json';
+const oxfordHURL = 'https://api.propublica.org/congress/v1/members/house/OH/8/current.json';
+const myHeaders = new Headers({
+    'x-api-key': PROPUBLICA_API_KEY
+});
+const myInit = {headers: myHeaders};
 
 function listSenateMembers(){
-
+    fetch(oxfordSURL, myInit)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(myJson) {
+        console.log(JSON.stringify(myJson));
+      });
 }
 
 function listHouseMembers(){
-
+    fetch(oxfordHURL, myInit)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(myJson) {
+        console.log(JSON.stringify(myJson));
+      });
 }
-
-listHouseMembers();
