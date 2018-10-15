f7864de33bfdf4e64d82d12a4b10c3588d03a260
Max Walters <max.walters10@gmail.com>
2018-10-15 17:02:04 -0400

    Trying to auto-populate senateURL()

--- api.js
+++ api.js
@@ -1,3 +1,7 @@
+/*
+    Address must be structured like "1234 Peaceful Ln. Cleveland OH"
+*/
+
 import 'isomorphic-fetch'
 
 const config = require('./config.js');
@@ -5,26 +9,36 @@ const PROPUBLICA_API_KEY = config.publica_KEY;
 const GOOGLE_API_KEY = config.google_KEY;
 var sURL = '';
 var hURL = '';
+var dist = 0;
 const publicaHead = new Headers({
     'x-api-key': PROPUBLICA_API_KEY
 });
 const publicaInit = {headers: publicaHead};
 
-function senateURL(){
-    sURL = 'https://api.propublica.org/congress/v1/members/senate/' + /* 2 letter state code */ + '/current.json';
+function senateURL(address) {
+    var stateTwo = getInfoGoogle(address);
+    sURL = 'https://api.propublica.org/congress/v1/members/senate/' + stateTwo + '/current.json';
+    console.log(sURL);
 }
 
-function houseURL(){
+function houseURL(address) {
     sURL = 'https://api.propublica.org/congress/v1/members/house/' + /* 2 letter state code */ + /* district number */ + '/current.json';
 }
 
-function getInfoGoogle(address){
-    var googURL = 'https://www.googleapis.com/civicinfo/v2/voterinfo?key=' + GOOGLE_API_KEY;
-    fetch(googURL) // working on fetch request to civic api
+function getInfoGoogle(address) {
+    address = addressForURL(address);
+    var googURL = 'https://www.googleapis.com/civicinfo/v2/voterinfo?key=' + GOOGLE_API_KEY + '&address=' + address;
+    fetch(googURL)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data){
+         return setState(data);
+      });
 }
 
-function listSenateMembers(){
-    fetch(oxfordSURL, publicaInit)
+function listSenateMembers(address) {
+    fetch(senateURL(address), publicaInit)
       .then(function(response) {
         return response.json();
       })
@@ -33,8 +47,8 @@ function listSenateMembers(){
       });
 }
 
-function listHouseMembers(){
-    fetch(oxfordHURL, publicaInit)
+function listHouseMembers(address) {
+    fetch(hURL, publicaInit)
       .then(function(response) {
         return response.json();
       })
@@ -42,3 +56,14 @@ function listHouseMembers(){
         console.log(JSON.stringify(myJson));
       });
 }
+
+function addressForURL(address) {
+    address = address.split(' ').join('%20');
+    return address;
+}
+
+function setState(data) {
+    return data.normalizedInput.state;
+}
+
+senateURL("8112 High Oaks Dr. Lambertville MI");
